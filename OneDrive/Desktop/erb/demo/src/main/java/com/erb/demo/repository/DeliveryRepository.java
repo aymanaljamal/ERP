1128baee59d43af083acfa303dcd6ac6b4f83ec9
𝙰𝚈𝙼𝙰𝙽 𝙰𝙻𝙹𝙰𝙼𝙰𝙻 <ayman.aljamal2017@gmail.com>
2025-07-17 15:05:50 +0300

    new SQL

--- OneDrive/Desktop/erb/demo/src/main/java/com/erb/demo/repository/DeliveryRepository.java
+++ OneDrive/Desktop/erb/demo/src/main/java/com/erb/demo/repository/DeliveryRepository.java
@@ -17,17 +17,25 @@ public interface DeliveryRepository extends JpaRepository<Delivery, Long> {
             w.name AS warehouseName,
             COUNT(d.id) AS totalDelivered,
             CASE
-                WHEN COUNT(d.id) > 10 THEN 'exceptional'
-                ELSE 'poor'
+                WHEN EXISTS (
+                    SELECT 1
+                    FROM delivery d2
+                    WHERE d2.employee_id = e.id
+                    AND (
+                        SELECT COUNT(*)
+                        FROM delivery d3
+                        WHERE d3.employee_id = e.id
+                        AND d3.delivered_at BETWEEN d2.delivered_at AND d2.delivered_at + INTERVAL 1 HOUR
+                    ) >= 10
+                ) THEN 'needs_raise'
+                ELSE 'normal'
             END AS status
         FROM employee e
         LEFT JOIN warehouse w ON e.warehouse_id = w.id
         LEFT JOIN delivery d ON d.employee_id = e.id
         GROUP BY e.id, e.name, e.email, w.name
         """,
-                countQuery = """
-        SELECT COUNT(*) FROM employee
-        """,
+                countQuery = "SELECT COUNT(*) FROM employee",
                 nativeQuery = true)
         Page<EmployeeSummaryProjection> getEmployeeSummary(Pageable pageable);
 }
