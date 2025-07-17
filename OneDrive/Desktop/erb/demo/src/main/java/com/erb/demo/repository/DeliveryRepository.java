3ca1fac78f7f3dcc460cff700f254d6cf1728b8a
𝙰𝚈𝙼𝙰𝙽 𝙰𝙻𝙹𝙰𝙼𝙰𝙻 <ayman.aljamal2017@gmail.com>
2025-07-17 12:13:08 +0300

    fix count

--- OneDrive/Desktop/erb/demo/src/main/java/com/erb/demo/repository/DeliveryRepository.java
+++ OneDrive/Desktop/erb/demo/src/main/java/com/erb/demo/repository/DeliveryRepository.java
@@ -2,32 +2,32 @@ package com.erb.demo.repository;
 
 import com.erb.demo.Projection.EmployeeSummaryProjection;
 import com.erb.demo.model.Delivery;
-import com.erb.demo.model.Employee;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
-import java.time.LocalDateTime;
-
 public interface DeliveryRepository extends JpaRepository<Delivery, Long> {
-        @Query(value = """
-    SELECT 
-        e.id AS id,
-        MAX(e.name) AS name,
-        MAX(e.email) AS email,
-        MAX(w.name) AS warehouseName,
-        COUNT(d.id) AS totalDelivered,
-        CASE 
-            WHEN COUNT(d.id) > 10 THEN 'exceptional'
-            ELSE 'poor'
-        END AS status
-    FROM delivery d
-    JOIN employee e ON d.employee_id = e.id
-    LEFT JOIN warehouse w ON e.warehouse_id = w.id
-    GROUP BY e.id
-    ORDER BY e.id
-    """, nativeQuery = true)
-        Page<EmployeeSummaryProjection> getEmployeeSummary(Pageable pageable);
 
+        @Query(value = """
+        SELECT
+            e.id AS id,
+            e.name AS name,
+            e.email AS email,
+            w.name AS warehouseName,
+            COUNT(d.id) AS totalDelivered,
+            CASE
+                WHEN COUNT(d.id) > 10 THEN 'exceptional'
+                ELSE 'poor'
+            END AS status
+        FROM employee e
+        LEFT JOIN warehouse w ON e.warehouse_id = w.id
+        LEFT JOIN delivery d ON d.employee_id = e.id
+        GROUP BY e.id, e.name, e.email, w.name
+        """,
+                countQuery = """
+        SELECT COUNT(*) FROM employee
+        """,
+                nativeQuery = true)
+        Page<EmployeeSummaryProjection> getEmployeeSummary(Pageable pageable);
 }
